6e14a16a7419538b5678dc8abf55a50bcfcf5efa
nathphrsaysud9-create <nathphrsaysud9@gmail.com>
2026-01-16 10:14:18 +0700

    Add confirmation message functionality in script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const messages = [
+  "Are you sure?",
+  "Really sure??",
+  "Are you positive?",
+  "Pookie please...",
+  "Just think about it!",
+  "I will be very sad...",
+  "Ok fine, I will stop asking...",
+  "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+  const noButton = document.querySelector('.no-button');
+  const yesButton = document.querySelector('.yes-button');
+
+  document.getElementById("clickSound").play();
+
+  noButton.textContent = messages[messageIndex];
+  messageIndex = (messageIndex + 1) % messages.length;
+
+  const currentSize = parseFloat(getComputedStyle(yesButton).fontSize);
+  yesButton.style.fontSize = `${currentSize * 1.3}px`;
+}
+
+function handleYesClick() {
+  document.getElementById("clickSound").play();
+  document.getElementById("yaySound").play();
+
+  document.getElementById("questionPage").classList.remove("show");
+  document.getElementById("yesPage").classList.add("show");
+}
